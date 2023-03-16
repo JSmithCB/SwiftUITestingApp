efa45cd697281952a325fa04080e85ebdfb8ea33
Josh Smith <josh.smith@chatbooks.com>
2023-03-15 22:32:07 -0500

    Initial Commit

---
+++ SwiftUITestingApplication/SwiftUITestingApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestingApplicationApp.swift
+//  SwiftUITestingApplication
+//
+//  Created by Josh Smith on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestingApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
